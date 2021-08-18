a213aab49f04f843094f34f37fe3284607cbb194
tkygtr6 <tkygtr6@gmail.com>
2021-08-19 00:23:28 +0900

    fix

--- scripts/run_after_bench.sh
+++ scripts/run_after_bench.sh
@@ -9,13 +9,6 @@ scp $SERVER2_HOSTNAME:~/.tmp/access.log logs/
 ssh $SERVER2_HOSTNAME "mkdir -p ~/.tmp; sudo cat /var/log/nginx/error.log > ~/.tmp/error.log"
 scp $SERVER2_HOSTNAME:~/.tmp/error.log logs/
 
-ssh $SERVER2_HOSTNAME "mkdir -p ~/.tmp; sudo cat /var/log/mysql/mysql-slow.sql > ~/.tmp/mysql-slow.sql"
-scp $SERVER2_HOSTNAME:~/.tmp/mysql-slow.sql logs/mysql-slow2.sql
-pt-query-digest logs/mysql-slow2.sql > logs/pt2.result
-
-ssh $SERVER2_HOSTNAME "mkdir -p ~/.tmp; sudo cat /var/log/mysql/mysql.log > ~/.tmp/mysql.log"
-scp $SERVER2_HOSTNAME:~/.tmp/mysql.log logs/mysql2.log
-
 
 ssh $SERVER3_HOSTNAME "mkdir -p ~/.tmp; sudo cat /var/log/mysql/mysql-slow.sql > ~/.tmp/mysql-slow.sql"
 scp $SERVER3_HOSTNAME:~/.tmp/mysql-slow.sql logs/mysql-slow3.sql
